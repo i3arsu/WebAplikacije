28484163564b4ee93d186750db12c65745b1e6f7
Luka Sever <lukkalukaa@gmail.com>
2022-03-18 09:22:41 +0100

    Zadaca 1 (API call implemented)

---
+++ zadaca/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+        <div>
+          <table>
+            <th>Testing</th>
+            <tr>Nesto</tr>
+            <tr v-for="item in list" v-bind:key="item.id">
+              <td>{{item[0]}}</td>
+            </tr>
+          </table>
+        </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name:"ApiCall",
+    data(){
+      return {list:undefined}
+    },
+    mounted(){
+        axios.get("https://api.github.com/repos/vuejs/vue/commits")
+        .then((resp)=>{
+            this.list=resp.data.data;
+            console.log(resp)
+        })
+
+    },
+}
+</script>
