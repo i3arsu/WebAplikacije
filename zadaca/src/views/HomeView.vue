b8a792eff7b58ab19371524223d635d4749de93b
Luka Sever <lukkalukaa@gmail.com>
2022-03-18 09:39:21 +0100

    Displaying response data

--- zadaca/src/views/HomeView.vue
+++ zadaca/src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <template>
         <div>
-          <table>
-            <th>Testing</th>
-            <tr>Nesto</tr>
-            <tr v-for="item in list" v-bind:key="item.id">
-              <td>{{item[0]}}</td>
-            </tr>
-          </table>
+          <div v-for="item in list" v-bind:key="item.id">
+            
+            <p>{{item}}</p>
+          
+          </div>
         </div>
 </template>
 
@@ -21,7 +19,7 @@ export default {
     mounted(){
         axios.get("https://api.github.com/repos/vuejs/vue/commits")
         .then((resp)=>{
-            this.list=resp.data.data;
+            this.list=resp.data;
             console.log(resp)
         })
 
