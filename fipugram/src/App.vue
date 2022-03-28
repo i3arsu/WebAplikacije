cdfb32abf7328067d94ab130a4e09cf113e11af1
Luka Sever <lukkalukaa@gmail.com>
2022-03-28 12:19:54 +0200

    Finished Vjezbe 02

--- fipugram/src/App.vue
+++ fipugram/src/App.vue
@@ -1,12 +1,57 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img
+          src="@/assets/tfpu.png"
+          alt=""
+          width="120"
+          class="d-inline-block align-text-top"
+        />TFPUgram
+      </a>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarToggler"
+        aria-controls="navbarToggler"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">Prijava</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/signup" class="nav-link"
+              >Izradi račun</router-link
+            >
+          </li>
+        </ul>
+
+        <form class="form-inline my-2 my-lg-0">
+          <input 
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+        </form>
+
+      </div>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
 
 <style lang="scss">
 #app {
@@ -17,8 +62,9 @@
   color: #2c3e50;
 }
 
-nav {
+#nav {
   padding: 30px;
+  background-color: white !important;
 
   a {
     font-weight: bold;
---
+++ fipugram/src/components/InstagramCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <div class="card-header text-left">Featured</div>
+    <div class="card-body p-0">
+      <img
+        class="card-img-top"
+        src="https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/1f469-200d-1f4bb.svg"
+        alt="Čovjek sa zlatnim zubima"
+      />
+    </div>
+    <div class="card-footer text-muted text-left"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InstagramCard",
+  props: {
+    msg: String,
+  },
+};
+</script>
