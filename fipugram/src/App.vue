aa4536f10b8e05dc4712ac034bdc641c71ef3a3d
Luka Sever <lukkalukaa@gmail.com>
2022-03-28 11:10:07 +0200

    Replaced vue3 with vue2

--- fipugram/src/App.vue
+++ fipugram/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/signup">Sign Up</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
